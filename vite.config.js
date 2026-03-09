40d6ae0d9bc446f9417fed2aab1f93897db2ff23
alasdairnc <alasdairnc@gmail.com>
2026-03-09 13:06:58 -0400

    feat: initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
     plugins: [
+      tailwindcss(),
       react(),
       {
         name: "api-dev-middleware",
